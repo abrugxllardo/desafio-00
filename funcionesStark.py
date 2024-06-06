4e53b518ced406608b49e1f3fee7daac834d1508
abrugxllardo <gallardoabril50@gmail.com>
2024-06-06 18:59:18 -0300

    segundo commit

--- funcionesStark.py
+++ funcionesStark.py
@@ -2,7 +2,7 @@
 from dataStark import * 
 def menu():
     limpiar_pantalla()
-    print(f"{'DESAFIO STARK 00':^50s}")
+    print(f"{'DESAFIO STARK':^50s}")
     print("A- Imprimir el nombre de cada superhéroe")
     print("B- Imprimir el nombre de cada superhéroe junto a la altura del mismo")
     print("C- Determinar cuál es el superhéroe más alto")
@@ -10,7 +10,22 @@ def menu():
     print("E- Determinar la altura promedio de los superhéroes")
     print("F- Informar cual es el Nombre del superhéroe segun el resultado de los puntos D y E")
     print("G- Calcular e informar cual es el superhéroe más y menos pesado")
-    print("H- Salir del programa")
+    print("H- Nombre de cada superhéroe de género M")
+    print("I- Nombre de cada superhéroe de género F")
+    print("J- Superhéroe más alto de género M")
+    print("K- Superhéroe más alto de género F")
+    print("L- Superhéroe más bajo de género M")
+    print("M- Superhéroe más bajo de género F")
+    print("N- Altura promedio de los superhéroes de género M")
+    print("O- Altura promedio de los superhéroes de género F")
+    print("P- Informar cual es el Nombre del superhéroe segun el resultado de los puntos de J a la N")
+    print("Q- Determinar cuántos superhéroes tienen cada tipo de color de ojos")
+    print("R- Determinar cuántos superhéroes tienen cada tipo de color de pelo")
+    print("S- Determinar cuántos superhéroes tienen cada tipo de inteligencia")
+    print("T- Listar todos los superhéroes agrupados por color de ojos.")
+    print("U- Listar todos los superhéroes agrupados por color de pelo.")
+    print("V- Listar todos los superhéroes agrupados por tipo de inteligencia")
+    print("W- Salir del programa ")
     return ingresar_opcion()
 
 def limpiar_pantalla():
@@ -33,7 +48,7 @@ def ingresar_opcion()->str:
     """
     opcion = input("Ingrese una opcion: ").upper()
 
-    while opcion not in "ABCDEFGH":
+    while opcion not in "ABCDEFGHIJKLMNOPQRSTUVW":
         opcion = input("Error, ingrese una opcion: ").upper()
 
     return opcion
@@ -58,7 +73,7 @@ def mostrar_heroe_altura(lista:list, key:str, key2:str):
         key2 (str): clave para buscar la altura
     """
     for heroe in lista:
-        print(f"- Nombre: {heroe[key]} - Altura: {heroe[key2]}")
+        print(f"- Nombre: {heroe[key]:<20} - Altura: {heroe[key2]:<20}")
 
 def heroe_mayor_dato(lista:list, key:str):
     """busca el dato maximo que sea pedido segun la clave
@@ -147,4 +162,177 @@ def pedirConfirmacion(mensaje:str)->str:
     """
     rta = input(mensaje).lower()
     return rta == "si"
+
+def superheroes_generos(lista:list, genero:str):
+    """
+    Imprime los nombres de los superhéroes que pertenecen al genero especificado
+
+    Args:
+        lista (list): Una lista de diccionarios que contienen informacion sobre supereroes
+        genero (str): El genero de los superhéroes que se quiera imprimir
+    """
+    lista_generos = []
+    for heroe in lista:
+        if heroe["genero"] == genero:
+            lista_generos.append(heroe["nombre"])
+
+    for heroe in lista_generos:
+        print(f"- {heroe}")
+
+def heroe_mas_alto_genero(lista:list, genero:str)-> float:
+    """
+    Encuentra la altura del superheroe mas alto del genero especificado
+
+    Args:
+        lista (list): Una lista de diccionarios que contienen informacion sobre superheroes
+        genero (str): El genero de los superheroes entre los cuales se busca al mas alto
+
+    Returns:
+        float: La altura del superheroe mas alto del genero especificado
+    """
+    mas_alto = 0
+    
+    for heroe in lista:
+        altura = float(heroe["altura"])
+        if heroe["genero"] == genero:
+            if altura > mas_alto:
+                mas_alto = altura
+            
+    return mas_alto
+            
+
+def heroe_mas_bajo_genero(lista:list, genero:str)-> float:
+    """
+    Encuentra la altura del superheroe mas bajo del genero especificado
+
+    Args:
+        lista (list): Una lista de diccionarios que contienen informacion sobre superheroes
+        genero (str): El genero de los superheroes entre los cuales se busca al mas bajo
+
+    Returns:
+        float: La altura del superheroe mas bajo del genero especificado
+    """
+    mas_bajo = float('inf')
+    
+    for heroe in lista:
+        altura = float(heroe["altura"])
+        if heroe["genero"] == genero:
+            if altura < mas_bajo:
+                mas_bajo = altura
+            
+    return mas_bajo
+
+def promedio_altura_genero(lista:list, genero:str)-> float:
+    """
+    Calcula el promedio de altura de los superheroes del genero especificado
+
+    Args:
+        lista (list): Una lista de diccionarios que contienen informacion sobre superheroes
+        genero (str): El genero de los superheroes para calcular el promedio de altura
+
+    Returns:
+        float: El promedio de altura de los superheroes del genero especificado
+    """
+    contador_heroes = 0
+    acumulador_altura = 0
+    promedio_total  = 0
+
+    for heroe in lista:
+        if heroe["genero"] == genero:
+            altura = float(heroe["altura"])
+            contador_heroes = len(lista)
+            acumulador_altura += altura
+        
+    if contador_heroes != 0:
+        promedio_total= acumulador_altura / contador_heroes
+    else: 
+        promedio_total = 0
+    return promedio_total
+
+def obtener_cantidad_por_dato(lista_heroes:list, key_dato:str):
+    """
+    Obtiene la cantidad de superheroes agrupados por un dato especifico
+
+    Args:
+        lista_heroes (list): Una lista de diccionarios que contienen informacion sobre superheroes
+        key_dato (str): El nombre del dato por el cual se quiere agrupar
+
+    Returns:
+        dict: Un diccionario que contiene la cantidad de superheroes agrupados por el dato especificado
+    """
+    if not lista_heroes:
+        return False
+
+    cantidad_por_dato = {}
+    for heroe in lista_heroes:
+        dato = heroe[key_dato]
+        if dato == "":
+            cantidad_por_dato["No tiene"] = cantidad_por_dato.get(dato, 0) + 1
+        else:
+            cantidad_por_dato[dato] = cantidad_por_dato.get(dato, 0) + 1
+
+    return cantidad_por_dato 
+
+def swapLista(lista:list, i :int, j:int):
+    """
+    Intercambia dos elementos en una lista
+
+    Args:
+        lista (list): La lista en la cual se realizan los intercambios
+        i (int): Indice del primer elemento a intercambiar
+        j (int): Indice del segundo elemento a intercambiar
+    """
+    aux = lista[i]
+    lista[i], lista[j] = lista[j], aux
+
+def ordenar_por_dato(lista:list, key:str):
+    """
+    Ordena una lista de diccionarios por el valor de una clave especifica
+
+    Args:
+        lista (list): Una lista de diccionarios que se desea ordenar
+        key (str): La clave por la cual se desea ordenar los diccionarios
+    """
+    cant_heroes = len(lista)
+    for i in range(cant_heroes - 1):
+        for j in range(i + 1, cant_heroes):
+            if lista[i][key] > lista[j][key] :
+                swapLista(lista, i ,j)
+
+
+def mostrar_heroes(lista:list, key:str):
+    """
+    Muestra una lista de superheroes y un atributo especifico de cada uno
+
+    Args:
+        lista (list): Una lista de diccionarios que contienen informacion sobre superheroes
+        key (str): La clave de los superheroes que se desea mostrar
+    """
+    tam = len(lista)
+    print("       ***LISTADO DE HEROES***   ")
+    print(f"Nombre              | {key.capitalize()} ")
+    print("------------------------------------------")
+    for i in range(tam):
+        mostrar_heroe(lista[i], key)
+    print()
+
+def mostrar_heroe(heroe:list, key):
+    """
+    Muestra la informacion de un superheroe
+
+    Args:
+        heroe (list): Una lista que contiene la informacion de un superheroe
+        key (str): La clave especifica del superheroe que se desea mostrar
+    """
+    if heroe[key] == "":
+        print(f"{heroe["nombre"]:<20}| {"No tiene":<9}")
+    else:
+        print(f"{heroe["nombre"]:<20}| {heroe[key]:<9}")
+
+
+
+
+
+
+
 
--- mainStark.py
+++ mainStark.py
@@ -25,6 +25,54 @@ while seguir == "si":
             peso_mas_alto = heroe_mayor_dato(lista_personajes, "peso")
             print(f"El heroe menos pesado es: {nombre_heroe(lista_personajes, "peso", peso_mas_bajo)}\nEl heroe mas pesado es: {nombre_heroe(lista_personajes,"peso", peso_mas_alto)}")
         case "H":
+            print("Superheroes masculinos: ")
+            superheroes_generos(lista_personajes, "M")
+        case "I":
+            print("Superheroes femenino: ")
+            superheroes_generos(lista_personajes, "F")
+        case "J":
+            
+            print(f"El superheore masculino mas alto mide: {heroe_mas_alto_genero(lista_personajes, "M")}cm")
+        case "K":
+            
+            print( f"El superheore femenino mas alto mide: {heroe_mas_alto_genero(lista_personajes, "F")}cm")
+        case "L":
+            
+            print(f"El superheore masculino mas bajo mide: {heroe_mas_bajo_genero(lista_personajes, "M")}cm")
+        case "M":
+            
+            print(f"El superheore femenino mas bajo mide: {heroe_mas_bajo_genero(lista_personajes, "F")}cm")
+        case "N":
+            promedio_altura_masc = f"El promedio de altura entre los superheroes masculinos es: {promedio_altura_genero(lista_personajes, "M")}"
+            print(promedio_altura_masc)
+        case "O":
+            promedio_altura_fem = f"El promedio de altura entre los superheroes femeninos es: {promedio_altura_genero(lista_personajes, "F")}"
+            print(promedio_altura_fem)
+        case "P":
+            masc_mas_alto = heroe_mas_alto_genero(lista_personajes, "M")
+            fem_mas_alto =heroe_mas_alto_genero(lista_personajes, "F")
+            masc_mas_bajo = heroe_mas_bajo_genero(lista_personajes, "M")
+            fem_mas_bajo = heroe_mas_bajo_genero(lista_personajes, "F")
+            print(f"El heroe masculino mas alto es: {nombre_heroe(lista_personajes, "altura", masc_mas_alto)}")
+            print(f"El heroe femenino mas alto es: {nombre_heroe(lista_personajes, "altura", fem_mas_alto)}")
+            print(f"El heroe masculino mas bajo es: {nombre_heroe(lista_personajes, "altura", masc_mas_bajo)}")
+            print(f"El heroe femenino mas bajo es: {nombre_heroe(lista_personajes, "altura", fem_mas_bajo)}")
+        case "Q":
+            print(obtener_cantidad_por_dato(lista_personajes, "color_ojos"))
+        case "R":
+            print(obtener_cantidad_por_dato(lista_personajes, "color_pelo"))
+        case "S":
+            print(obtener_cantidad_por_dato(lista_personajes, "inteligencia"))
+        case "T":
+            print(ordenar_por_dato(lista_personajes, "color_ojos"))
+            mostrar_heroes(lista_personajes, "color_ojos")
+        case "U":
+            print(ordenar_por_dato(lista_personajes, "color_pelo"))
+            mostrar_heroes(lista_personajes, "color_pelo")
+        case "V":
+            print(ordenar_por_dato(lista_personajes, "inteligencia"))
+            mostrar_heroes(lista_personajes, "inteligencia")
+        case "W":
             if pedirConfirmacion("Confirmar salida si/no: "):
                 seguir = "no"
             continue
